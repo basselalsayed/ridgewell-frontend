22201ace9e6bd85429b82ee521d35e7732a4a862
basselalsayed <alsayed.bassel@gmail.com>
2020-07-14 09:56:32 +0100

    continued for users

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Users from './users';
+import { Users } from './users';
 
 const Home = () => (
   <div>
--- src/components/users/user/index.js
+++ src/components/users/user/index.js
@@ -1,16 +1,1 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
-
-const User = ({ email, id, password, updatedAt, username }) => (
-  <Card>
-    <Card.Title>{username}</Card.Title>
-    <Card.Body>
-      <p> {id} </p>
-      <p> {email} </p>
-      <p> {password} </p>
-      <p> {updatedAt} </p>
-    </Card.Body>
-  </Card>
-);
-
-export default User;
+export * from './user';
