4bf3f7892220812f7c49252921d8186a7b9ad9d8
basselalsayed <alsayed.bassel@gmail.com>
2020-07-14 13:50:00 +0100

    menu burger button

--- src/components/BoardAdmin.js
+++ src/components/BoardAdmin.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 import { Holidays } from './holidays';
-
+import classes from './menu.module.css';
 const BoardAdmin = () => (
   <div className='container'>
+    <div className={classes.menuBtn}>
+      <div className={classes.menuBtnBurger}></div>
+    </div>
     <header className='jumbotron'>
       <Holidays />
     </header>
