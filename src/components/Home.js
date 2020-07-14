b00fe09898a4728811033f8895804878ee2ae672
basselalsayed <alsayed.bassel@gmail.com>
2020-07-14 08:18:45 +0100

    users component extracted out

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 
-import { Card } from 'react-bootstrap';
 import { getUsers } from '../store/actions/content';
-import { connect, useSelector } from 'react-redux';
-import authHeader from '../store/actions/auth-header';
-import axios from 'axios';
+import { connect } from 'react-redux';
 
-const UsersBase = ({ users }) =>
-  users && (
-    <div>
-      {users.map(user => (
-        <User key={user.id} {...user} />
-      ))}
-    </div>
-  );
-
-const mapStateToProps = state => ({
-  users: state.contentReducer.users,
-});
-
-const Users = connect(mapStateToProps)(UsersBase);
-
-const User = ({ email, id, password, updatedAt, username }) => (
-  <Card>
-    <Card.Title>{username}</Card.Title>
-    <Card.Body>
-      <p> {id} </p>
-      <p> {email} </p>
-      <p> {password} </p>
-      <p> {updatedAt} </p>
-    </Card.Body>
-  </Card>
-);
+import Users from './users';
 
 const Home = ({ getUsers }) => {
   useEffect(() => {
