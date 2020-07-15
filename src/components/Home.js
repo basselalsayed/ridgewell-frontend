e1f3cfc71cc9c656b03ff1f898a5f871358d06b3
basselalsayed <alsayed.bassel@gmail.com>
2020-07-15 02:12:37 +0100

    calendar initial state working

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Users } from './users';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-const Home = () => (
-  <div>
-    {/* <header className='jumbotron'> */}
-    <Users />
-    {/* </header> */}
-  </div>
-);
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import { useSelector, useDispatch } from 'react-redux';
+import moment from 'moment';
+import { getHolidays } from '../actions';
+
+const localizer = momentLocalizer(moment);
+
+const Home = () => {
+  const { holidays } = useSelector(state => state.contentReducer);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getHolidays());
+  }, []);
+
+  let array = [];
+
+  holidays &&
+    holidays.forEach(({ from, until, owner }) =>
+      array.push({ title: owner, start: new Date(from), end: new Date(until) }),
+    );
+
+  return (
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={array}
+        startAccessor='start'
+        endAccessor='end'
+        style={{ height: 500 }}
+      />
+    </div>
+  );
+};
+// <header className='jumbotron'>
+// </header>
 
 export { Home };
