8df6b7fe5a27fa97c4e7238a57caf4724858db07
basselalsayed <alsayed.bassel@gmail.com>
2020-10-31 15:35:31 +0000

    added decryptor helper

---
+++ src/helpers/decryptor.js
@@ -0,0 +1,20 @@
+import crypto from 'crypto';
+
+const [algorithm, hex, iv, key, utf8] = [
+  'aes-256-cbc',
+  'hex',
+  process.env.REACT_APP_MY_IV,
+  process.env.REACT_APP_MY_SECRET_KEY,
+  'utf-8',
+];
+
+const decrypt = encrypted => {
+  let decipher = crypto.createDecipheriv(algorithm, key, iv);
+
+  let decrypted = decipher.update(encrypted, hex, utf8);
+  decrypted += decipher.final(utf8);
+
+  return decrypted;
+};
+
+export { decrypt };
