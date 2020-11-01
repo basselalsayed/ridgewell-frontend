2a2d7191ed2bbf235e8a03729a99f9527ae70e42
basselalsayed <alsayed.bassel@gmail.com>
2020-11-01 18:09:44 +0000

    getusers, getHolidays - axios defaults, instance

--- src/store/actions/content.js
+++ src/store/actions/content.js
@@ -1,22 +1,22 @@
 import { API_URL } from '../../constants';
 import axios from 'axios';
 import authHeader from '../../services/auth-header';
+import { holidayInstance } from '../../services/axios';
 
 const setContent = (type, payload) => ({ type, payload });
 
 const getUsers = () => async dispatch =>
   await axios
-    .get(API_URL + 'users', { headers: authHeader() })
+    .get('users')
     .then(({ data: users }) => dispatch(setContent('SET_USERS', users)));
 
 const getHolidays = () => async dispatch =>
-  await axios
-    .get(API_URL + 'holidays', {
-      headers: authHeader(),
+  await holidayInstance
+    .get()
+    .then(({ data }) => {
+      dispatch(setContent('SET_HOLIDAYS', data));
     })
-    .then(({ data }) => dispatch(setContent('SET_HOLIDAYS', data)))
     .catch(error => console.log(error, error.response));
-
 const getRequests = () => async dispatch =>
   await axios
     .get(API_URL + 'requests', {
