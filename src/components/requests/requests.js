3df4ef9b55b854156c25f3372157f457cc6daaf5
basselalsayed <alsayed.bassel@gmail.com>
2020-11-04 15:36:53 +0000

    removed deprecated gets in content components

--- src/components/requests/requests.js
+++ src/components/requests/requests.js
@@ -1,16 +1,10 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { Request } from './request';
 import { Spinner } from 'react-bootstrap';
-import { getRequests } from '../../store/actions';
 
 const Requests = () => {
   const { requests } = useSelector(state => state.contentReducer);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getRequests());
-  }, []);
 
   return requests ? (
     <div
