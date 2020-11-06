c940d9f3c59bf982dccf110b22c409c19602f118
basselalsayed <alsayed.bassel@gmail.com>
2020-11-06 15:25:28 +0000

    cleaned up CountdownCancel

--- src/components/forms/CountdownCancel.js
+++ src/components/forms/CountdownCancel.js
@@ -4,54 +4,35 @@ import { useFormikContext } from 'formik';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { CountdownCircleTimer } from 'react-countdown-circle-timer';
-import { endCountdown, getHolidays } from '../../store/actions';
+import { endCountdown } from '../../store/actions';
 import './countdown.css';
-import axios from 'axios';
 
-const CountdownCancel = ({ holidayId }) => {
+const CountdownCancel = () => {
   const dispatch = useDispatch();
-  const { setStatus, setSubmitting, submitForm } = useFormikContext();
+  const { submitForm } = useFormikContext();
+  const { isPlaying } = useSelector(state => state.countdownReducer);
 
-  const { isPlaying, isDelete } = useSelector(state => state.countdownReducer);
-
-  const handleDelete = async () => {
-    await axios
-      .post('requests', { holidayId, type: 'delete' })
-      .then(({ data: { message } }) => setStatus(message))
-      .catch(err => {
-        setStatus(`${err.response.statusText}: ${err.response.data.message}`);
-      })
-      .finally(() => setSubmitting(false));
-
-    dispatch(getHolidays());
-  };
+  const cancelButton = (
+    <button className='countdownBtn' onClick={() => dispatch(endCountdown())}>
+      <div className='countdownText'>Cancel</div>
+    </button>
+  );
 
   return (
-    isPlaying && (
-      <div className='countdownWrp'>
-        <CountdownCircleTimer
-          children={
-            <button
-              className='countdownBtn'
-              onClick={() => {
-                dispatch(endCountdown());
-              }}
-            >
-              <div className='countdownText'>Cancel</div>
-            </button>
-          }
-          isPlaying={isPlaying}
-          duration={1}
-          size={90}
-          strokeWidth={5}
-          colors={[['#004777', 0.33], ['#F7B801', 0.33], ['#A30000']]}
-          onComplete={() => {
-            submitForm();
-            dispatch(endCountdown());
-          }}
-        />
-      </div>
-    )
+    <div className='countdownWrp'>
+      <CountdownCircleTimer
+        children={cancelButton}
+        isPlaying={isPlaying}
+        duration={5}
+        size={90}
+        strokeWidth={5}
+        colors={[['#004777', 0.33], ['#F7B801', 0.33], ['#A30000']]}
+        onComplete={() => {
+          submitForm();
+          dispatch(endCountdown());
+        }}
+      />
+    </div>
   );
 };
 
