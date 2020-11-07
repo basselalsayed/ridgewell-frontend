a906794581b88437802198425e95bd958d71e92c
basselalsayed <alsayed.bassel@gmail.com>
2020-11-07 15:35:01 +0000

    added requests table to Content - User

--- src/components/users/user/user.js
+++ src/components/users/user/user.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import { formatted } from '../../../helpers';
+import { RequestsTable } from '../../home/event/components';
 
-const User = ({ email, updatedAt, username }) => (
-  <Card>
-    <Card.Title>{username}</Card.Title>
-    <Card.Body>
-      <p> Email: {email} </p>
-      <p> Updated: {updatedAt} </p>
-    </Card.Body>
-  </Card>
-);
+const User = ({ email, updatedAt, username, owner: requests }) => {
+  return (
+    <Card>
+      <Card.Title>{username}</Card.Title>
+      <Card.Body>
+        <p> Email: {email} </p>
+        <p> Updated: {formatted(updatedAt, 'panelTime')} </p>
+        <RequestsTable requests={requests} />
+      </Card.Body>
+    </Card>
+  );
+};
 
 export { User };
