ef456810b0a20a6611127dd175b10e0a56ac2f76
basselalsayed <alsayed.bassel@gmail.com>
2020-11-09 11:21:32 +0000

    holidays iterating through prop

--- src/components/holidays/holidays.js
+++ src/components/holidays/holidays.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { Holiday } from './holiday';
-import { CenteredSpinner } from '../';
 
-const Holidays = () => {
-  const { holidays } = useSelector(state => state.contentReducer);
-
-  return holidays ? (
-    <div
-      style={{
-        maxHeight: window.innerHeight - 150,
-        overflow: 'auto',
-      }}
-    >
-      {holidays.map(hol => (
-        <Holiday key={hol.id} {...hol} />
-      ))}
-    </div>
-  ) : (
-    <CenteredSpinner />
-  );
-};
+const Holidays = ({ holidays }) => (
+  <div
+    style={{
+      maxHeight: window.innerHeight - 150,
+      overflow: 'auto',
+    }}
+  >
+    {holidays.map(hol => (
+      <Holiday key={hol.id} {...hol} />
+    ))}
+  </div>
+);
 
 export { Holidays };
