20a7a7520033d1dba908b09add8e5b4d54b08ff8
basselalsayed <alsayed.bassel@gmail.com>
2020-11-09 11:20:51 +0000

    users iterating through prop

--- src/components/users/users.js
+++ src/components/users/users.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { User } from './user';
 
-const Users = () => {
-  const { users } = useSelector(state => state.contentReducer);
-
-  return (
-    users && (
-      <div
-        style={{
-          maxHeight: window.innerHeight - 150,
-          overflow: 'auto',
-        }}
-      >
-        {users.map(user => (
-          <User key={user.id} {...user} />
-        ))}
-      </div>
-    )
-  );
-};
+const Users = ({ users }) => (
+  <div
+    style={{
+      maxHeight: window.innerHeight - 150,
+      overflow: 'auto',
+    }}
+  >
+    {users.map(user => (
+      <User key={user.id} {...user} />
+    ))}
+  </div>
+);
 
 export { Users };
