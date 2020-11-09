4a4b6d5a42e05d711b91a2fc19a0c068bb94838a
basselalsayed <alsayed.bassel@gmail.com>
2020-11-09 23:20:55 +0000

    cleaned up requestsTable

--- src/components/home/event/components/RequestsTable.js
+++ src/components/home/event/components/RequestsTable.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { Table } from 'react-bootstrap';
 import { capitalize, formatted } from '../../../../helpers';
 
+const RequestRow = ({ from, index, type, until }) => (
+  <tr>
+    <td>{index + 1}</td>
+    <td>{type && capitalize(type)}</td>
+    {<td>{from && formatted(from, 'popover')}</td>}
+    {<td>{until && formatted(until, 'popover')}</td>}
+  </tr>
+);
+
 const RequestsTable = ({ requests }) => {
   const headerRow = (
     <thead>
@@ -14,19 +23,16 @@ const RequestsTable = ({ requests }) => {
     </thead>
   );
 
-  const requestRows = requests.map(({ id, type, from, until }, idx) => (
-    <tr key={id}>
-      <td>{idx + 1}</td>
-      <td>{type && `${capitalize(type)}`}</td>
-      {<td>{from && formatted(from, 'popover')}</td>}
-      {<td>{until && formatted(until, 'popover')}</td>}
-    </tr>
-  ));
+  const requestRows =
+    requests &&
+    requests.map((request, idx) => (
+      <RequestRow key={request.id} {...request} index={idx} />
+    ));
 
   return (
     <Table striped bordered hover size='sm'>
       {headerRow}
-      {requestRows}
+      <tbody>{requestRows}</tbody>
     </Table>
   );
 };
