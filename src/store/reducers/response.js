2f1e4abf2fee553f672f30e28e3f29209cbe1200
basselalsayed <alsayed.bassel@gmail.com>
2020-11-09 18:32:36 +0000

    added show to response reducer

--- src/store/reducers/response.js
+++ src/store/reducers/response.js
@@ -1,15 +1,17 @@
 const initialState = {
   error: '',
+  show: false,
   success: '',
 };
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
     case 'SET_ERROR':
-      return { ...state, error: payload };
+      return { ...state, error: payload, show: true };
     case 'SET_MESSAGE':
-      return { ...state, success: payload };
-
+      return { ...state, success: payload, show: true };
+    case 'HIDE_ALERT':
+      return { ...state, show: false };
     default:
       return state;
   }
