48940e2b1fef0088c3976b2c1205905333508617
basselalsayed <alsayed.bassel@gmail.com>
2020-11-09 14:27:39 +0000

    export isOwner

--- src/helpers/user.js
+++ src/helpers/user.js
@@ -4,4 +4,4 @@ const isOwner = ({ id }, ownerId) => id === ownerId;
 
 const hasEditAcces = (user, ownerId) => isOwner(user, ownerId) || isAdmin(user);
 
-export { isAdmin, hasEditAcces };
+export { isAdmin, isOwner, hasEditAcces };
