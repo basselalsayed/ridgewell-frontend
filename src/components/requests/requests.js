9589e01eb976b77d68073760f86f5d9a9190dc59
basselalsayed <alsayed.bassel@gmail.com>
2020-11-09 11:20:05 +0000

    requests iterating through prop

--- src/components/requests/requests.js
+++ src/components/requests/requests.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { Request } from './request';
-import { CenteredSpinner } from '../Spinner';
 
-const Requests = () => {
-  const { requests } = useSelector(state => state.contentReducer);
-
-  return requests ? (
-    <div
-      style={{
-        maxHeight: window.innerHeight - 150,
-        overflow: 'auto',
-      }}
-    >
-      {requests.map(req => (
-        <Request key={req.id} {...req} />
-      ))}
-    </div>
-  ) : (
-    <CenteredSpinner />
-  );
-};
+const Requests = ({ requests }) => (
+  <div
+    style={{
+      maxHeight: window.innerHeight - 150,
+      overflow: 'auto',
+    }}
+  >
+    {requests.map(req => (
+      <Request key={req.id} {...req} />
+    ))}
+  </div>
+);
 
 export { Requests };
