f25a6141ed159ab614501a7afd256dbfea3ba8e1
basselalsayed <alsayed.bassel@gmail.com>
2020-11-09 23:27:30 +0000

    error handling in getContent actions

--- src/store/actions/content.js
+++ src/store/actions/content.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { parseError } from '../../helpers';
 import { decryptorInstance, usersInstance } from '../../services/axios';
 import { setError, setSuccess } from './response';
 
@@ -8,25 +9,25 @@ const getUsers = () => async dispatch =>
   await usersInstance
     .get()
     .then(({ data }) => dispatch(setContent('SET_USERS', data)))
-    .catch(error => console.log(error, error.response));
+    .catch(error => dispatch(setError(parseError(error))));
 
 const getHolidays = userId => async dispatch =>
   await decryptorInstance
     .get(userId ? `holidays?userId=${userId}` : 'holidays')
     .then(({ data }) => dispatch(setContent('SET_HOLIDAYS', data)))
-    .catch(error => console.log(error, error.response));
+    .catch(error => dispatch(setError(parseError(error))));
 
 const getRequests = userId => async dispatch =>
   await decryptorInstance
     .get(userId ? `requests?userId=${userId}` : 'requests')
     .then(({ data }) => dispatch(setContent('SET_REQUESTS', data)))
-    .catch(error => console.log(error, error.response));
+    .catch(error => dispatch(setError(parseError(error))));
 
 const getNotifications = () => async dispatch =>
   await decryptorInstance
     .get('notifications')
     .then(({ data }) => dispatch(setContent('SET_NOTIFICATIONS', data)))
-    .catch(error => console.log(error, error.response));
+    .catch(error => dispatch(setError(parseError(error))));
 
 const getAll = userId => async dispatch =>
   await Promise.all([
@@ -40,13 +41,7 @@ const updateNotification = (id, read) => async dispatch =>
   await axios
     .put(`/notifications/${id}`, { read: !read })
     .then(({ data: { message } }) => dispatch(setSuccess(message)))
-    .catch(error => {
-      let errorText = '';
-      if (error.statusText) errorText += error.statusText;
-      if (error.response.message) errorText += ` ${error.response.message}`;
-      if (!error.statusText && !error.response.data.message) errorText = error;
-      dispatch(setError(errorText));
-    });
+    .catch(error => dispatch(setError(parseError(error))));
 
 const postHolidays = params => {};
 
